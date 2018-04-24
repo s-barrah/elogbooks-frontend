95c014e19859af00ea48cc6d9c220d91f079ffdc
Neo Neo <neothedeveloper231@gmail.com>
2018-04-24 21:41:17 +0100

    Updated Quote frontend routing

--- app/modules/quote/quote.routing.js
+++ app/modules/quote/quote.routing.js
@@ -18,15 +18,16 @@
                 controllerAs: 'vm',
                 templateUrl: '/modules/quote/list/list.html',
                 resolve: {
-                    quoteCollectionResponse : function () {
-                        return [
-                            { id:1, description: 'Carpet deep clean' },
-                            { id:2, description: 'New carpet in reception' },
-                            { id:3, description: 'New entrance doors' },
-                            { id:4, description: 'Fixing toilet leak' },
-                            { id:5, description: 'New parts required for boiler' },
-                            { id:6, description: 'New office equipment' }
-                        ]
+                    quoteCollectionResponse : function ($http) {
+                        return $http({
+                            url: 'http://localhost:8001/quote',
+                            method: "GET",
+                            params: {}
+                        }).then(function (response) {
+                            return response.data;
+                        }, function () {
+                            console.log('Request Failed');
+                        });
                     }
                 }
             })
